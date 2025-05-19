4d630c6e230ae74397022d2ebd15a1b29aec5737
Margad19 <21b1num0631@stud.num.edu.mn>
2025-05-19 13:20:44 +0800

    fdaf

--- travelfy-server/routes/upload.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const { uploadProcess } = require("../controllers/upload.controller");
-const uploadCloud = require("../helpers/cloudinary");
-
-router.post("/upload", uploadCloud.single("images"), uploadProcess);
-
-module.exports = router;
--- travelfy-server/utils/consts.js
+++
@@ -1,3 +0,0 @@
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/travelfy-back";
-
-module.exports = MONGODB_URI;
